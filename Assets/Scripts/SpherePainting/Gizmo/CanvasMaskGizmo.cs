bec7b3e8bdd6d93512fc818e8f715a8468281bc3
Reo Tanaka <168517869+tanakareo@users.noreply.github.com>
2025-04-24 18:06:55 +0900

    キャンバスギズモのバグを修正

--- Assets/Scripts/SpherePainting/Gizmo/CanvasMaskGizmo.cs
+++ Assets/Scripts/SpherePainting/Gizmo/CanvasMaskGizmo.cs
@@ -7,13 +7,13 @@ namespace SpherePainting
     {
         [SerializeField] private ViewportRendering m_ViewportRendering;
         [SerializeField] private Canvas m_Canvas;
-        [SerializeField] private RenderTexture m_SourceRenderTexture;
-        [SerializeField] private RenderTexture m_DestinationRenderTexture;
+        [SerializeField] private RenderTexture m_SourceRenderTexture; // ギズモ適用前のテクスチャ
+        [SerializeField] private RenderTexture m_DestinationRenderTexture; // ギズモを適用するテクスチャ
 
         private Material m_Material;
 
-        private readonly ReactiveProperty<float> m_Opacity = new (1.0f);
-        private readonly ReactiveProperty<bool> m_IsActive = new (false);
+        private readonly ReactiveProperty<float> m_Opacity = new (1.0f); // ギズモの不透明度
+        private readonly ReactiveProperty<bool> m_IsActive = new (false); // ギズモを表示するかどうか
 
         public override ReadOnlyReactiveProperty<float> Opacity => m_Opacity;
         public override ReadOnlyReactiveProperty<bool> IsActive => m_IsActive;
@@ -23,14 +23,18 @@ namespace SpherePainting
         private void Awake()
         {
             m_Material = new Material(Shader.Find("Hidden/CanvasMaskShader"));
+
+            // キャンバスのパラメータが変更されたら、ギズモの描画を更新
             m_Canvas.CurrentShapeType.Subscribe(v =>
             {
                 bool isEllipse = m_Canvas.CurrentShapeType.CurrentValue == Canvas.ShapeType.ELLIPSE;
                 m_Material.SetFloat("_IsEllipse", isEllipse ? 1.0f : 0.0f);
+                m_IsUpdateRequired = true;
             }).AddTo(this);
             m_Canvas.CurrentTextureScale.Subscribe(v =>
             {
                 m_Material.SetVector("_Scale", v);
+                m_IsUpdateRequired = true;
             }).AddTo(this);
             m_Canvas.CurrentTextureOffset.Subscribe(v =>
             {
@@ -39,6 +43,7 @@ namespace SpherePainting
             }).AddTo(this);
             m_Material.SetFloat("_Strength", m_Opacity.Value);
 
+            // レンダリングが完了したとき、タイルレンダリングでタイルの描画が終わったときにギズモの描画を更新
             m_ViewportRendering.OnRenderProgress += () =>
             {
                 m_IsUpdateRequired = true;
